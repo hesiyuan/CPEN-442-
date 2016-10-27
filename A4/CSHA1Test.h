d42cc9e0ffb45cdb1e313f07500dda9544f18542
hesiyuan <hesiyuan48@gmail.com>
2016-10-26 18:13:21 -0700

    die

--- A4/CSHA1Test.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHA1_TEST_H_441F47FDA9C14DA9810A6DA3416F1580
-#define SHA1_TEST_H_441F47FDA9C14DA9810A6DA3416F1580
-
-std::string HashString(std::string s);
-void HashFile();
-void crackPassword();
-void crackPassword2();
-
-#endif // SHA1_TEST_H_441F47FDA9C14DA9810A6DA3416F1580
